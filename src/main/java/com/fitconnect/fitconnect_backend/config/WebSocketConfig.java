ae4bb805d4a9baf316d9d5e15655575488a356e6
Praneethha <160530815+PraneethhaRB@users.noreply.github.com>
2026-08-01 10:55:02 +0530

    Update WebSocketConfig.java

--- src/main/java/com/fitconnect/fitconnect_backend/config/WebSocketConfig.java
+++ src/main/java/com/fitconnect/fitconnect_backend/config/WebSocketConfig.java
@@ -24,12 +24,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.setApplicationDestinationPrefixes("/app");
     }
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:5173")
-                .withSockJS();
-    }
+   @Override
+public void registerStompEndpoints(StompEndpointRegistry registry) {
+    registry.addEndpoint("/ws")
+            .setAllowedOriginPatterns(
+                "http://localhost:5173",
+                "https://fit-connect-frontend.vercel.app"
+            )
+            .withSockJS();
+}
     @Override
 public void configureClientInboundChannel(ChannelRegistration registration) {
     registration.interceptors(webSocketAuthInterceptor);
